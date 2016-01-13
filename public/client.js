6663287076485363b04617ee284c487029a55c76
Mimi Schatz <mcschatz@gmail.com>
2016-01-13 11:00:47 -0700

    users can cast votes, see how many users are connected, and see their connection status

---
+++ public/client.js
@@ -0,0 +1,24 @@
+var socket = io();
+
+var connectionCount = document.getElementById('connection-count');
+var statusMessage = document.getElementById('status-message');
+
+socket.on('usersConnected', function(count) {
+  connectionCount.innerText = 'Connected Users: ' + count;
+});
+
+socket.on('statusMessage', function(message) {
+  statusMessage.innerText = message;
+});
+
+socket.on('voteCount', function (votes) {
+  console.log(votes);
+});
+
+var buttons = document.querySelectorAll('#choices button');
+
+for (var i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', function () {
+    socket.send('voteCast', this.innerText);
+  });
+}
